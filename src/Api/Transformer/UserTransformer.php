0100b11dd9f9919f29907e7cae8e1f639ca30b70
Renato Todorov <renato.todorov@rocket-internet.com.br>
2014-11-19 11:11:52 -0200

    DocBlock for UserTransform

--- src/Api/Transformer/UserTransformer.php
+++ src/Api/Transformer/UserTransformer.php
@@ -6,6 +6,13 @@ use League\Fractal;
 
 class UserTransformer extends Fractal\TransformerAbstract
 {
+	/**
+	 * Indicates to Fractal how to transform the User model into a json
+	 *
+	 * @param User $user       User entity.
+	 *
+	 * @return array
+	 */
 	public function transform(User $user)
 	{
 	    return [
